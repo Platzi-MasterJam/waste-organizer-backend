50d03b316b159b458c42303b28fd24f7fc6e3c50
Angel Flores Ayala <cyberangel1000@gmail.com>
2021-06-12 18:45:32 -0500

    Added support for CORS

--- app/main.py
+++ app/main.py
@@ -3,6 +3,8 @@ from api.routes.api import router as api_router
 # from core.config import API_PREFIX, DEBUG, PROJECT_NAME, VERSION
 # from core.events import create_start_app_handler
 from fastapi import FastAPI
+from fastapi.middleware.cors import CORSMiddleware
+
 
 def get_application() -> FastAPI:
     application = FastAPI()# (title=PROJECT_NAME, debug=DEBUG, version=VERSION)
@@ -11,3 +13,11 @@ def get_application() -> FastAPI:
 
 
 app = get_application()
+
+app.add_middleware(
+    CORSMiddleware,
+    allow_origins=["*"],
+    allow_credentials=True,
+    allow_methods=["*"],
+    allow_headers=["*"],
+)
